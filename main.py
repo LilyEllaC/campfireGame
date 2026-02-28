4acb2375966b021ac3c8bbb09002339485e5ece0
Llily Ella C <lilyellacallard@gmail.com>
2026-02-28 15:24:34 -0500

    basic stuff

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+import utility as util
+import constants as const
+# pylint: disable=no-member
+
+pygame.init()
+
+#variables
+running=True
+clock=pygame.time.Clock()
+pygame.display.set_caption("Campfire Game")
+
+
+
+def main():
+    global running
+
+    while running:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                running=False
+        const.SCREEN.fill(const.BLACK)
+        util.toScreen("Hi", const.FONT20, const.RED, 500,500)
+        pygame.display.flip()
+        clock.tick(const.FPS)
+
+if __name__ == "__main__":
+    main()
+    pygame.quit()
+
---
+++ utility.py
@@ -0,0 +1,23 @@
+import pygame
+import constants as const
+
+#to screen
+def toScreen(words, font, colour, x, y):
+    text=font.render(words, True, colour)
+    textRect=text.get_rect()
+    textRect.center=(x,y)
+    const.SCREEN.blit(text, textRect)
+def toScreen2(words1, words2, font, colour, x, y):
+    toScreen(words1, font, colour, x, y - font.get_height() // 2)
+    toScreen(words2, font, colour, x, y + font.get_height() // 2)
+def toScreen3(words1, words2, words3, font, colour, x, y):
+    toScreen(words1, font, colour, x, y - font.get_height())
+    toScreen(words2, font, colour, x, y)
+    toScreen(words3, font, colour, x, y + font.get_height())
+
+#images
+def imageToScreen(imageName, x, y, width, height):
+    image = pygame.image.load(imageName)
+    image = pygame.transform.scale(image, (width, height))
+    const.SCREEN.blit(image, (x, y))
+
