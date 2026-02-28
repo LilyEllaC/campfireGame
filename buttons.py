3d1716fcad49e2d289e18dfbac0dd8ae4295dd16
Llily Ella C <lilyellacallard@gmail.com>
2026-02-28 16:42:43 -0500

    button is better now

--- buttons.py
+++ buttons.py
@@ -13,16 +13,22 @@ class Button:
         self.colour=colour1
         self.buttonRect=pygame.Rect(x, y, width, height)
         self.button=pygame.draw.rect(const.SCREEN, self.colour, self.buttonRect)
-        self.triangleP1X=self.x+self.width/2*5
+        pygame.draw.rect(const.SCREEN, const.BLACK, self.buttonRect, 3)
+
+        self.triangleP1X=self.x+self.width/7*2
         self.triangleP1Y=self.y+self.height/8
-        self.triangleP2X=self.x+self.width/2*5
-        self.triangleP2Y=self.y+self.height/7*8
-        self.triangleP3X=self.x+self.width/3*5
+        self.triangleP2X=self.x+self.width/7*2
+        self.triangleP2Y=self.y+self.height/8*7
+        self.triangleP3X=self.x+self.width/7*5
         self.triangleP3Y=self.y+self.height/2
-        self.trainglePoints=[(self.traingleP1X, self.triangleP1Y), (self.traingleP2X, self.triangleP2Y), (self.traingleP3X, self.triangleP3Y)]
+        self.trianglePoints=[(self.triangleP1X, self.triangleP1Y), (self.triangleP2X, self.triangleP2Y), (self.triangleP3X, self.triangleP3Y)]
+        pygame.draw.polygon(const.SCREEN, const.BLACK, self.trianglePoints)
+
 
     def display(self):
         self.button=pygame.draw.rect(const.SCREEN, self.colour, self.buttonRect)
+        pygame.draw.polygon(const.SCREEN, const.BLACK, self.trianglePoints)
+        pygame.draw.rect(const.SCREEN, const.BLACK, self.buttonRect, 3)
 
     def checkMouse(self):
         mouseX, mouseY=pygame.mouse.get_pos()
