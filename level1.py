e2c5044af8e493e1f3f13a0761a6b425754040f5
mridhula101 <2586976@cegep-heritage.qc.ca>
2026-03-01 03:16:28 -0500

    kinda finished level1

--- level1.py
+++ level1.py
@@ -5,30 +5,58 @@ import sprites
 import playing_music as play
 
 obstacles = pygame.sprite.Group()
-size = 50
+height = 100
+width =150
 #creating obstacles
 def create_maze():
+    x_Val = 1
+
  #top
+    for i in range(1,12):
+        if i!= 13:
+            obstacles.add(sprites.Hinder(x_Val,0, width, height, True,0))
+        
+        x_Val = x_Val +width
 
-    obstacles.add(sprites.Hinder(0,0, 350, size, True,0))
-    obstacles.add(sprites.Hinder(450, 0, 350, size, True,0))
+          
+#left
+y_Val = 2 
+for i in range(1, 10):
+    if i not in (5, 6):
+         obstacles.add(sprites.Hinder(1, y_Val, width, height, True, 0))
+    
+    y_Val = y_Val + height
+  
+#bootom
+    x_Val = 1
+    for i in range(1,12):
+        obstacles.add(sprites.Hinder(x_Val,const.HEIGHT - height, width, height, True,0))
+        x_Val = x_Val +width
+#right
+    y_Val = 2 
+    for i in range(1, 10):
+        if i not in (2,3):
+             obstacles.add(sprites.Hinder(const.WIDTH - width, y_Val, width, height, True, 0))
+    
+        y_Val = y_Val + height
 
-    #bootom
-    obstacles.add(sprites.Hinder(0, const.HEIGHT - W, 350, W, True, 0))
-    obstacles.add(sprites.Hinder(450, const.HEIGHT - W, 350, W, True, 0))
-
-    #side
-    obstacles.add(sprites.Hinder(0, 0,w,const.HEIGHT,True, 0))
-    obstacles.add(sprites.Hinder(const.WIDTH - W, 0, W, const.HEIGHT, True, 0))
-
-
+#middle
+    for i in (3,5,7,9):
+        x_px = i * width
+        for j in range(1,8):
+           if i == 3 and j in (3, 7): continue
+           if i == 5 and j in (4, 5): continue
+           if i == 7 and j in (2, 8): continue
+           if i == 9 and j == 5: continue
+           
+           obstacles.add(sprites.Hinder(x_px, j *height, width, height, True, 0))
 
 def playLevel(player):
 #     #music
 #     play.CurrentPage("other")
    #const.SCREEN.fill("assets\Backround floor2.jpg")
     util.imageToScreen("assets/Backround floor2.jpg", 0, 0,const.WIDTH, const.HEIGHT)
-
+    create_maze()
 #    util.imageToScreen("assets\Backround floor2.jpg")
     for obstacle in obstacles:
         obstacle.display()
