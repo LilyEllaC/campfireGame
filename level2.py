51005c0c89ec037d4cbedfea46a198a619589bef
LilyEllaC <lilyellacallard@gmail.com>
2026-03-01 06:08:13 -0500

    example object added

--- level2.py
+++ level2.py
@@ -8,11 +8,15 @@ size=50
 obstacles.add(sprites.Hinder(10,300, 350, size, False,1))
 obstacles.add(sprites.Hinder(450, 700, 350, size, False,1))
 
+#objects
+key=sprites.Object(450, 300, 40, 40, "assets/key.png", 1)
 
 def playLevel(player):
     util.imageToScreen("assets/backround floor2 dark.png", 0, 0,const.WIDTH, const.HEIGHT)
 
 #    util.imageToScreen("assets\Backround floor2.jpg")
+    key.display()
+    key.collide(player)
     for obstacle in obstacles:
         obstacle.display()
     player.collisions(obstacles)
