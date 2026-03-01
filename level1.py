a22fb4982af56fbda7fa6ba0c87cfc115d6ac871
mridhula101 <2586976@cegep-heritage.qc.ca>
2026-03-01 04:58:20 -0500

    new push

--- level1.py
+++ level1.py
@@ -6,65 +6,73 @@ import playing_music as play
 
 obstacles = pygame.sprite.Group()
 height = 100
-width =150
-#creating obstacles
+door = None
+width = 150
+
+def val(x,y,w,h):
+    return sprites.Door(x,y,w,h)
+
+
+# creating obstacles
 def create_maze():
+    obstacles.empty()
     x_Val = 1
 
- #top
-    for i in range(1,12):
-        if i!= 13:
-            obstacles.add(sprites.Hinder(x_Val,0, width, height, True,0))
-        
-        x_Val = x_Val +width
+    # top
+    for i in range(1, 12):
+        obstacles.add(sprites.Hinder(x_Val, 0, width, height, True, 0))
+        x_Val = x_Val + width
 
-   #left
-    y_Val = 2 
+    # left 
+    y_Val = height 
     for i in range(1, 10):
         if i not in (5, 6):
-         obstacles.add(sprites.Hinder(1, y_Val, width, height, True, 0))
-    
-    y_Val = y_Val + height
+            obstacles.add(sprites.Hinder(1, y_Val, width, height, True, 0))
+        y_Val = y_Val + height 
   
-#bootom
+    # bottom
     x_Val = 1
-    for i in range(1,12):
-        obstacles.add(sprites.Hinder(x_Val,const.HEIGHT - height, width, height, True,0))
-        x_Val = x_Val +width
-#right
-    y_Val = 2 
+    for i in range(1, 12):
+        obstacles.add(sprites.Hinder(x_Val, const.HEIGHT - height, width, height, True, 0))
+        x_Val = x_Val + width
+
+    # right 
+    y_Val = height 
     for i in range(1, 10):
-        if i not in (2,3):
-             obstacles.add(sprites.Hinder(const.WIDTH - width, y_Val, width, height, True, 0))
-    
+       
+        if i not in (2, 3, 8):
+            obstacles.add(sprites.Hinder(const.WIDTH - width, y_Val, width, height, True, 0))
+        
+        if i == 8:
+            door=val(const.WIDTH - width, y_Val, width, height)
+
         y_Val = y_Val + height
-
-
-#middle
-    for i in (3,5,7,9):
+        
+    # middle 
+    for i in (3, 5, 7):
         x_px = i * width
-        for j in range(1,8):
-           if i == 3 and j in (3, 7): continue
-           if i == 5 and j in (4, 5): continue
-           if i == 7 and j in (2, 8): continue
-           if i == 9 and j == 5: continue
-           
-           obstacles.add(sprites.Hinder(x_px, j *height, width, height, True, 0))
+        for j in range(1, 10):
+            if i == 3 and j in (3,4): continue
+            if i == 5 and j in (4, 5): continue
+            if i == 7 and j in (2, 3): continue
+            
+            obstacles.add(sprites.Hinder(x_px, j * height, width, height, True, 0))
 
+# Initialize objects
+# door=sprites.Door(const.WIDTH//2-350, const.HEIGHT/2-50, 150, 300)
 
-
-door=sprites.Door(const.WIDTH//2-350, const.HEIGHT/2-50, 150, 300)
 create_maze()
+
 def playLevel(player):
-#     #music
-#     play.CurrentPage("other")
-   #const.SCREEN.fill("assets\Backround floor2.jpg")
-    util.imageToScreen("assets/Backround floor2.jpg", 0, 0,const.WIDTH, const.HEIGHT)
+    util.imageToScreen("assets/Backround floor2.jpg", 0, 0, const.WIDTH, const.HEIGHT)
     
-#    util.imageToScreen("assets\Backround floor2.jpg")
     for obstacle in obstacles:
         obstacle.display()
+    #if door is not None:
     door.display()
+    
     player.collisions(obstacles)
-    return door.collide(player, 1)
 
+        
+    #if door:
+    return door.collide(player, 1)
